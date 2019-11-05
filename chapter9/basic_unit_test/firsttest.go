7311cf0360788c51ca7cc9603ca681874bfa9f47
Prithvipal Singh <prithvipal@oc0421167372.ibm.com>
2019-11-05 23:55:45 +0530

    first test case

---
+++ chapter9/basic_unit_test/firsttest.go
@@ -0,0 +1,31 @@
+package basic_unit_test
+
+import (
+	"net/http"
+	"testing"
+)
+
+const checkMark = '\u2713'
+const ballotX = '\u2717'
+
+func TestDownload(t *testing.T) {
+	url := "http://www.goinggo.net/feeds/posts/default?alt=rss"
+	statusCode := 200
+
+	t.Log("Given the need to test downloading content.")
+	{
+		t.Logf("\t When checking \"%s\" for status code \"%d\"", url, statusCode)
+		{
+			resp, err := http.Get(url)
+			if err != nil {
+				t.Fatal("\t\tShould be able to make the GET call.", ballotX, err)
+			}
+			defer resp.Body.Close()
+			if resp.StatusCode == statusCode {
+				t.Logf("\t\tShould receive a \"%d\" status. %v", statusCode, checkMark)
+			} else {
+				t.Errorf("\t\tShould receive a \"%d\" status. %v %v", statusCode, ballotX, resp.StatusCode)
+			}
+		}
+	}
+}
